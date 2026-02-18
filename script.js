06df68869e2ee59a3b06496d3526f11a900462b7
Q1OST <q1ost72512@gmail.com>
2026-02-18 10:11:49 +0300

    1.01

--- script.js
+++ script.js
@@ -151,7 +151,7 @@ if (bookingForm) {
     }
 
     try {
-      const response = await fetch("https://qwefsdfsdsg-mdk.hf.space", {
+      const response = await fetch("https://qwefsdfsdsg-mdk.hf.spaceapi/reservations", {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
