3a24a7cafc13193798372ea1ee079fed259ab765
q1est <q1est7251@gmail.com>
2026-05-16 21:18:27 +0200

    Update logs.go

--- back/logs/logs.go
+++ back/logs/logs.go
@@ -6,17 +6,18 @@ import (
 	"net"
 	"net/http"
 	"os"
+	"time"
 )
 
 func Log() {
 	err := os.Mkdir("backend_logs", os.ModePerm)
 	if err != nil {
-		log.Println("[WARN]not dir log",err)
+		log.Println("[WARN]not dir log", err)
 
 	}
 	logFile, err := os.OpenFile("backend_logs/log.txt", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
 	if err != nil {
-		log.Println("[WARN]didn't open file ",err)
+		log.Println("[WARN]didn't open file ", err)
 	}
 	multiwriter := io.MultiWriter(os.Stdout, logFile)
 	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
@@ -25,23 +26,29 @@ func Log() {
 }
 func GetIP(r *http.Request) string {
 	ip := r.Header.Get("X-Forwarded-For")
-	if ip != "" {return ip 	} 
+	if ip != "" {
+		return ip
+	}
 	ip = r.Header.Get("X-Real-IP")
-	if ip != "" { 
-		return ip 	 
-	} 
-	host,_,err := net.SplitHostPort(r.RemoteAddr) 
-	if err !=nil{return  r.RemoteAddr}  
+	if ip != "" {
+		return ip
+	}
+	host, _, err := net.SplitHostPort(r.RemoteAddr)
+	if err != nil {
+		return r.RemoteAddr
+	}
 	return host
-} 
-func LogMiddleware(next http.Handler) http.Handler {
+}
+func LogMiddleware(next http.Handler) http.Handler { 
+	start := time.Now()
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		ip := GetIP(r)
-		log.Printf("[REQUEST] method = %s path = %s ip = %s browser = %s", 
-		r.Method,
-		r.URL.Path,
-		ip, 
-		r.UserAgent(),
+		log.Printf("[REQUEST] method = %s path = %s ip = %s user_agent = %s latency = %s",
+			r.Method,
+			r.URL.Path,
+			ip,
+			r.UserAgent(),
+			time.Since(start),
 		)
 	})
 }
