aa73bd2d4b4e94082514a40939c0775bd3d8ec24
Q1OST <q1ost72512@gmail.com>
2026-06-10 01:11:46 +0300

    .

--- js/cartModal.js
+++ js/cartModal.js
@@ -1,36 +1,86 @@
-import { openModal, closeModal, bindOverlayClose } from "./modal.js";
-import { cart, clearCart } from "./cart.js";
-import { updateCart } from "./cartUI.js";
+import {
+  openModal,
+  closeModal,
+  bindOverlayClose
+} from "./modal.js";
 
-const cartBtn = document.getElementById("cartBtn");
-const cartModal = document.getElementById("cartModal");
-const closeCart = document.getElementById("closeCart");
-const clearCartBtn = document.getElementById("clearCart");
+import {
+  cart,
+  clearCart
+} from "./cart.js";
 
-if (cartBtn) {
-  cartBtn.addEventListener("click", () => {
-    updateCart();
-    openModal(cartModal);
-  });
-}
+import { updateCart }
+  from "./cartUI.js";
 
-if (closeCart) {
-  closeCart.addEventListener("click", () => {
-    closeModal(cartModal);
-  });
-}
+document.addEventListener(
+  "DOMContentLoaded",
+  () => {
 
-bindOverlayClose(cartModal);
+    const cartBtn =
+      document.getElementById("cartBtn");
 
-if (clearCartBtn) {
-  clearCartBtn.addEventListener("click", () => {
-    if (Object.keys(cart).length === 0) {
-      alert("Корзина уже пуста ☢️");
-      return;
+    const cartModal =
+      document.getElementById("cartModal");
+
+    const closeCart =
+      document.getElementById("closeCart");
+
+    const clearCartBtn =
+      document.getElementById("clearCart");
+
+    if (cartBtn) {
+
+      cartBtn.addEventListener(
+        "click",
+        () => {
+
+          updateCart();
+
+          openModal(cartModal);
+        }
+      );
     }
-    if (confirm("Очистить корзину?")) {
-      clearCart();
-      updateCart();
+
+    if (closeCart) {
+
+      closeCart.addEventListener(
+        "click",
+        () => {
+          closeModal(cartModal);
+        }
+      );
     }
-  });
-}
+
+    bindOverlayClose(cartModal);
+
+    if (clearCartBtn) {
+
+      clearCartBtn.addEventListener(
+        "click",
+        () => {
+
+          if (
+            Object.keys(cart).length === 0
+          ) {
+
+            alert(
+              "Корзина уже пуста ☢️"
+            );
+
+            return;
+          }
+
+          if (
+            confirm(
+              "Очистить корзину?"
+            )
+          ) {
+
+            clearCart();
+
+            updateCart();
+          }
+        }
+      );
+    }
+});
